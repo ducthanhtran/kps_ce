4ba72c892aa093fd3748844389df072790867d76
Duc Thanh Tran <5289997+ducthanhtran@users.noreply.github.com>
2020-05-12 17:39:43 +0200

    Added update mechanism with average of sampled vectors

--- src/bernoulli.rs
+++ src/bernoulli.rs
@@ -1,4 +1,6 @@
-use ndarray::{Array1, Array2};
+use std::mem;
+
+use ndarray::{Array1, Array2, Axis};
 
 use rand::distributions::{Bernoulli, Distribution};
 use rand::rngs::StdRng;
@@ -34,6 +36,15 @@ impl BernoulliProduct {
             samples.extend(self.sample(rng).iter());
         }
         Array2::from_shape_vec((sample_size, self.dim()), samples).unwrap()
+    }
+
+    /// best_samples has dimensionality (#best_samples, self.dim())
+    pub fn update(&mut self, best_samples: Array2<f64>) {
+        let mean = best_samples.mean_axis(Axis(0)).unwrap();
+        for i in 0..self.dim() {
+            self.parameters[i] = mean[i];
+            mem::replace(&mut self.distributions[i], Bernoulli::new(mean[i]).unwrap());
+        }
     }
 }
 
@@ -72,6 +83,24 @@ mod tests {
         assert_eq!(sample_matrix.shape(), &[sample_size, dim]);
         for &v in sample_matrix.iter() {
             assert!(0f64 <= v && v <= 1f64);
+        }
+    }
+
+    #[test]
+    fn test_bernoulli_product_dist_update() {
+        let dim = 5;
+        let mut dist = BernoulliProduct::new(dim);
+
+        let best_samples = ndarray::arr2(&[[1., 0., 1., 1., 0.],
+                                                                           [0., 1., 1., 0., 0.],
+                                                                           [1., 1., 1., 0., 0.]]);
+        dist.update(best_samples);
+        assert_eq!(dist.parameters, ndarray::arr1(&[2./3., 2./3., 1., 1./3., 0.]));
+
+        let mut rng = StdRng::seed_from_u64(32);
+        for _ in 0..10 {
+            assert_eq!(dist.sample(&mut rng)[2], 1.);
+            assert_eq!(dist.sample(&mut rng)[4], 0.);
         }
     }
 }
